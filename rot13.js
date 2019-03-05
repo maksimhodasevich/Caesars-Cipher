96b1bd4640798a0275de46145508c434d1dc9d58
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2019-03-06 01:25:05 +0300

    rot13

---
+++ rot13.js
@@ -0,0 +1,20 @@
+function rot13(str) {
+  str = str.toLowerCase();
+  var alp = "abcdefghijklmnopqrstuvwxyz";  
+  var answer = '';
+  for(var i = 0; i < str.length; i++){
+    if(alp.indexOf(str[i]) > 12){
+      answer += alp[12 - (25 - alp.indexOf(str[i]))];
+    }
+    else if(alp.indexOf(str[i]) > -1){
+      answer += alp[alp.indexOf(str[i])+13];  
+    }
+    else {
+      answer += str[i];
+    }
+    
+    
+  }
+  answer = answer.toUpperCase();
+  return console.log(answer);
+}
